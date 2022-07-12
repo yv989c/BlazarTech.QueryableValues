116a8c13f2fcc2c43a9aad82604d2c6b1832f85e
yv989c <yv989c@gmail.com>
2022-07-11 23:07:50 -0400

    fix: avoids the logging of redundant warnings when initialized

--- src/QueryableValues.SqlServer/ModelCustomizer.cs
+++ src/QueryableValues.SqlServer/ModelCustomizer.cs
@@ -1,6 +1,7 @@
 ﻿#if EFCORE
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 
 namespace BlazarTech.QueryableValues
@@ -15,9 +16,21 @@ namespace BlazarTech.QueryableValues
             _previousModelCustomizer = previousModelCustomizer;
         }
 
-        private static void SetupEntity<T>(ModelBuilder modelBuilder)
+        /// <summary>
+        /// Used to satisfice model validation and remove the SqlServerEventId.DecimalTypeDefaultWarning warning from the logs.
+        /// </summary>
+        private static void SetDefaultPrecision<T>(PropertyBuilder<T> property)
         {
-            modelBuilder
+#if EFCORE3
+            property.HasColumnType("decimal(18, 6)");
+#else
+            property.HasPrecision(18, 6);
+#endif
+        }
+
+        private static EntityTypeBuilder<QueryableValuesEntity<T>> SetupEntity<T>(ModelBuilder modelBuilder)
+        {
+            return modelBuilder
                 .Entity<QueryableValuesEntity<T>>()
                 // By mapping to a fake view, we stop EF from including these entities during
                 // SQL generation in migrations and by the Create and Drop apis in DbContext.Database.
@@ -28,11 +41,25 @@ namespace BlazarTech.QueryableValues
         private static void SetupEntity(ModelBuilder modelBuilder)
         {
             modelBuilder
-                .Entity<QueryableValuesEntity>()
-                // By mapping to a fake view, we stop EF from including these entities during
-                // SQL generation in migrations and by the Create and Drop apis in DbContext.Database.
-                .ToView(nameof(QueryableValuesEntity))
-                .HasNoKey();
+                .Entity<QueryableValuesEntity>(entity =>
+                {
+                    SetDefaultPrecision(entity.Property(p => p.Decimal0));
+                    SetDefaultPrecision(entity.Property(p => p.Decimal1));
+                    SetDefaultPrecision(entity.Property(p => p.Decimal2));
+                    SetDefaultPrecision(entity.Property(p => p.Decimal3));
+                    SetDefaultPrecision(entity.Property(p => p.Decimal4));
+                    SetDefaultPrecision(entity.Property(p => p.Decimal5));
+                    SetDefaultPrecision(entity.Property(p => p.Decimal6));
+                    SetDefaultPrecision(entity.Property(p => p.Decimal7));
+                    SetDefaultPrecision(entity.Property(p => p.Decimal8));
+                    SetDefaultPrecision(entity.Property(p => p.Decimal9));
+
+                    // By mapping to a fake view, we stop EF from including these entities during
+                    // SQL generation in migrations and by the Create and Drop apis in DbContext.Database.
+                    entity
+                        .ToView(nameof(QueryableValuesEntity))
+                        .HasNoKey();
+                });
         }
 
         public void Customize(ModelBuilder modelBuilder, DbContext context)
@@ -41,7 +68,12 @@ namespace BlazarTech.QueryableValues
             SetupEntity<short>(modelBuilder);
             SetupEntity<int>(modelBuilder);
             SetupEntity<long>(modelBuilder);
-            SetupEntity<decimal>(modelBuilder);
+
+            var decimalProperty = SetupEntity<decimal>(modelBuilder)
+                .Property(p => p.V);
+
+            SetDefaultPrecision(decimalProperty);
+
             SetupEntity<float>(modelBuilder);
             SetupEntity<double>(modelBuilder);
             SetupEntity<DateTime>(modelBuilder);
