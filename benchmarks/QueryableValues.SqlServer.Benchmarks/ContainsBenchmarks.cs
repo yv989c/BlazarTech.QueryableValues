b7fc494677e36e82139515539ab06978b13447be
yv989c <yv989c@gmail.com>
2023-03-26 01:47:46 -0400

    chore: benchmarks without vs. with-xml vs. with-json

--- benchmarks/QueryableValues.SqlServer.Benchmarks/ContainsBenchmarks.cs
+++ benchmarks/QueryableValues.SqlServer.Benchmarks/ContainsBenchmarks.cs
@@ -4,22 +4,43 @@ using BenchmarkDotNet.Engines;
 using BlazarTech.QueryableValues;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
 
 namespace QueryableValues.SqlServer.Benchmarks;
 
-[SimpleJob(RunStrategy.Monitoring, warmupCount: 1, targetCount: 25, invocationCount: 200)]
+//[SimpleJob(RunStrategy.Monitoring, warmupCount: 1, iterationCount: 25, invocationCount: 200)]
+//[SimpleJob(RunStrategy.Monitoring, warmupCount: 1, iterationCount: 6, invocationCount: 200)]
+[SimpleJob(RunStrategy.Monitoring, warmupCount: 1, iterationCount: 6, invocationCount: 32)]
 [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
 [GcServer(true), MemoryDiagnoser]
 public class ContainsBenchmarks
 {
-#pragma warning disable CS8618
-    private IQueryable<Int32Entity> _int32Query;
-    private IQueryable<GuidEntity> _guidQuery;
-    private IQueryable<Int32Entity> _queryableValuesInt32Query;
-    private IQueryable<GuidEntity> _queryableValuesGuidQuery;
-#pragma warning restore CS8618
+    private IQueryable<Int32Entity> _int32Query = default!;
+    private IQueryable<GuidEntity> _guidQuery = default!;
+    private IQueryable<StringEntity> _stringQuery = default!;
 
-    [Params(2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096)]
+    private IQueryable<Int32Entity> _queryableValuesJsonInt32Query = default!;
+    private IQueryable<GuidEntity> _queryableValuesJsonGuidQuery = default!;
+    private IQueryable<StringEntity> _queryableValuesJsonStringQuery = default!;
+
+    private IQueryable<Int32Entity> _queryableValuesXmlInt32Query = default!;
+    private IQueryable<GuidEntity> _queryableValuesXmlGuidQuery = default!;
+    private IQueryable<StringEntity> _queryableValuesXmlStringQuery = default!;
+
+    public enum DataType
+    {
+        Int32,
+        Guid,
+        String
+    }
+
+    [Params(DataType.Int32, DataType.Guid, DataType.String)]
+    //[Params(DataType.String)]
+    public DataType Type { get; set; }
+
+    //[Params(512)]
+    //[Params(2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096)]
+    [Params(2, 8, 32, 128, 512, 2048)]
     public int NumberOfValues { get; set; }
 
     private IEnumerable<int> GetIntValues()
@@ -38,26 +59,59 @@ public class ContainsBenchmarks
         }
     }
 
+    private IEnumerable<string> GetStringValues()
+    {
+        var sb = new StringBuilder();
+
+        for (int i = 0; i < NumberOfValues; i++)
+        {
+            sb.Clear();
+            var length = Random.Shared.Next(0, 50);
+            for (int x = 0; x < length; x++)
+            {
+                sb.Append((char)Random.Shared.Next(32, 126));
+            }
+            yield return sb.ToString();
+        }
+    }
+
     [GlobalSetup]
     public void GlobalSetup()
     {
         Console.WriteLine("Initializing...");
 
-        var dbContext = new MyDbContext();
+        var dbContextXml = new MyDbContext(SerializationOptions.UseXml);
+        var dbContextJson = new MyDbContext(SerializationOptions.UseJson);
 
         #region Init db
         {
-            var wasCreated = dbContext.Database.EnsureCreated();
+            var wasCreated = dbContextXml.Database.EnsureCreated();
 
             if (wasCreated)
             {
-                for (int i = 0; i < 1000; i++)
+                const int itemsCount = 1000;
+
+                for (int i = 0; i < itemsCount; i++)
                 {
-                    dbContext.Add(new Int32Entity());
-                    dbContext.Add(new GuidEntity());
+                    dbContextXml.Add(new Int32Entity());
+                    dbContextXml.Add(new GuidEntity());
                 }
 
-                dbContext.SaveChanges();
+                var i2 = 0;
+
+                foreach (var value in GetStringValues())
+                {
+                    i2++;
+
+                    dbContextXml.Add(new StringEntity { Id = $"{value}{i2}" });
+
+                    if (i2 == itemsCount)
+                    {
+                        break;
+                    }
+                }
+
+                dbContextXml.SaveChanges();
             }
 
             var versionParam = new SqlParameter("@Version", System.Data.SqlDbType.NVarChar, -1)
@@ -65,11 +119,11 @@ public class ContainsBenchmarks
                 Direction = System.Data.ParameterDirection.Output
             };
 
-            dbContext.Database.ExecuteSqlRaw("SET @Version = @@VERSION;", versionParam);
+            dbContextXml.Database.ExecuteSqlRaw("SET @Version = @@VERSION;", versionParam);
 
             Console.WriteLine(versionParam.Value);
 
-            dbContext.Database.ExecuteSqlRaw("DBCC FREEPROCCACHE; DBCC DROPCLEANBUFFERS;");
+            dbContextXml.Database.ExecuteSqlRaw("DBCC FREEPROCCACHE; DBCC DROPCLEANBUFFERS;");
         }
         #endregion
 
@@ -77,11 +131,14 @@ public class ContainsBenchmarks
         {
             var intValues = GetIntValues();
 
-            _int32Query = dbContext.Int32Entities
+            _int32Query = dbContextXml.Int32Entities
                 .Where(i => intValues.Contains(i.Id));
 
-            _queryableValuesInt32Query = dbContext.Int32Entities
-                .Where(i => dbContext.AsQueryableValues(intValues).Contains(i.Id));
+            _queryableValuesXmlInt32Query = dbContextXml.Int32Entities
+                .Where(i => dbContextXml.AsQueryableValues(intValues).Contains(i.Id));
+
+            _queryableValuesJsonInt32Query = dbContextJson.Int32Entities
+                .Where(i => dbContextJson.AsQueryableValues(intValues).Contains(i.Id));
         }
         #endregion
 
@@ -89,36 +146,87 @@ public class ContainsBenchmarks
         {
             var guidValues = GetGuidValues();
 
-            _guidQuery = dbContext.GuidEntities
+            _guidQuery = dbContextXml.GuidEntities
                 .Where(i => guidValues.Contains(i.Id));
 
-            _queryableValuesGuidQuery = dbContext.GuidEntities
-                .Where(i => dbContext.AsQueryableValues(guidValues).Contains(i.Id));
+            _queryableValuesXmlGuidQuery = dbContextXml.GuidEntities
+                .Where(i => dbContextXml.AsQueryableValues(guidValues).Contains(i.Id));
+
+            _queryableValuesJsonGuidQuery = dbContextJson.GuidEntities
+                .Where(i => dbContextJson.AsQueryableValues(guidValues).Contains(i.Id));
+        }
+        #endregion
+
+        #region String Queries
+        {
+            var stringValues = GetStringValues();
+
+            _stringQuery = dbContextXml.StringEntities
+                .Where(i => stringValues.Contains(i.Id));
+
+            _queryableValuesXmlStringQuery = dbContextXml.StringEntities
+                .Where(i => dbContextXml.AsQueryableValues(stringValues, true).Contains(i.Id));
+
+            _queryableValuesJsonStringQuery = dbContextJson.StringEntities
+                .Where(i => dbContextJson.AsQueryableValues(stringValues, true).Contains(i.Id));
         }
         #endregion
     }
 
-    [Benchmark(Baseline = true), BenchmarkCategory("Int32")]
-    public void Without_Int32()
+    [Benchmark(Baseline = true)]
+    public void Without()
     {
-        _int32Query.Any();
+        switch (Type)
+        {
+            case DataType.Int32:
+                _int32Query.Any();
+                break;
+            case DataType.Guid:
+                _guidQuery.Any();
+                break;
+            case DataType.String:
+                _stringQuery.Any();
+                break;
+            default:
+                throw new NotImplementedException();
+        }
     }
 
-    [Benchmark, BenchmarkCategory("Int32")]
-    public void With_Int32()
+    [Benchmark]
+    public void WithXml()
     {
-        _queryableValuesInt32Query.Any();
+        switch (Type)
+        {
+            case DataType.Int32:
+                _queryableValuesXmlInt32Query.Any();
+                break;
+            case DataType.Guid:
+                _queryableValuesXmlGuidQuery.Any();
+                break;
+            case DataType.String:
+                _queryableValuesXmlStringQuery.Any();
+                break;
+            default:
+                throw new NotImplementedException();
+        }
     }
 
-    [Benchmark(Baseline = true), BenchmarkCategory("Guid")]
-    public void Without_Guid()
+    [Benchmark]
+    public void WithJson()
     {
-        _guidQuery.Any();
-    }
-
-    [Benchmark, BenchmarkCategory("Guid")]
-    public void With_Guid()
-    {
-        _queryableValuesGuidQuery.Any();
+        switch (Type)
+        {
+            case DataType.Int32:
+                _queryableValuesJsonInt32Query.Any();
+                break;
+            case DataType.Guid:
+                _queryableValuesJsonGuidQuery.Any();
+                break;
+            case DataType.String:
+                _queryableValuesJsonStringQuery.Any();
+                break;
+            default:
+                throw new NotImplementedException();
+        }
     }
 }
--- benchmarks/QueryableValues.SqlServer.Benchmarks/MyDbContext.cs
+++ benchmarks/QueryableValues.SqlServer.Benchmarks/MyDbContext.cs
@@ -1,20 +1,27 @@
 ﻿using BlazarTech.QueryableValues;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace QueryableValues.SqlServer.Benchmarks
 {
     public class MyDbContext : DbContext
     {
-#pragma warning disable CS8618
-        public DbSet<Int32Entity> Int32Entities { get; set; }
-        public DbSet<GuidEntity> GuidEntities { get; set; }
-#pragma warning restore CS8618
+        private readonly SerializationOptions _serializationOptions;
+
+        public DbSet<Int32Entity> Int32Entities { get; set; } = default!;
+        public DbSet<GuidEntity> GuidEntities { get; set; } = default!;
+        public DbSet<StringEntity> StringEntities { get; set; } = default!;
+
+        public MyDbContext(SerializationOptions serializationOptions)
+        {
+            _serializationOptions = serializationOptions;
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
-                @"Server=.\SQLEXPRESS;Integrated Security=true;Database=QueryableValuesBenchmarks",
-                builder => builder.UseQueryableValues()
+                @"Server=.\SQLEXPRESS;Integrated Security=true;Database=QueryableValuesBenchmarks;Encrypt=False;",
+                builder => builder.UseQueryableValues(options => options.Serialization(_serializationOptions))
                 );
         }
 
@@ -32,5 +39,11 @@ namespace QueryableValues.SqlServer.Benchmarks
     public class GuidEntity
     {
         public Guid Id { get; set; }
+    }
+
+    public class StringEntity
+    {
+        [MaxLength(100)]
+        public string Id { get; set; } = default!;
     }
 }
